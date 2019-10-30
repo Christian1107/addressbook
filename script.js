69a198290b39f39d2320dd3ee26858ad128d95fa
Christian Christian <christianchristian1107@gmail.com>
2019-10-30 08:55:45 -0400

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-class AddressBook {
-  constructor() {
-    this.contacts = [];
-  }
-  add(name, email, phone, relation) {
-    this.contacts.push(new Contact(name, email, phone, relation));
-  }
-  deleteAt(index) {
-    this.contacts.splice(index, 1);
-  }
-}
-
 class Contact {
   constructor(name, email, phone, relation) {
     this.name = name;
@@ -19,19 +7,86 @@ class Contact {
   }
 }
 
-function print(book) {
-  for (const contact of book.contacts) {
-    console.log(book);
+class AddressBook {
+  constructor() {
+    this.contacts = [];
+  }
+  add(name, email, phone, relation) {
+    this.contacts = [
+      ...this.contacts,
+      new Contact(name, email, phone, relation)
+    ];
+  }
+  deleteAt(index) {
+    index = Number(index);
+    this.contacts = [
+      ...this.contacts.slice(0, index),
+      ...this.contacts.slice(index + 1)
+    ];
   }
 }
-const book = new AddressBook();
 
-book.add("Chris", "hdbfhd", "355336", "djfj");
-book.add("Chris", "hdbfhd", "355336", "djfj");
-book.add("Chris", "hdbfhd", "355336", "djfj");
-book.add("Chris", "hdbfhd", "355336", "djfj");
+function print(addressBookReference) {
+  addressBookReference.contacts.forEach(contact => {
+    console.log(contact);
+  });
+}
 
-console.log(book.contacts.length);
-console.log(book.contacts[0].name);
-console.log(book);
-book.deleteAt(1);
+const addressBook = new AddressBook();
+addressBook.add("Mitch", "mitch@grandcircus.co", "248-248-2487", "me");
+addressBook.add("Tyler", "tyler@email.com", "248-248-2488", "brother");
+addressBook.add("John", "john@email.com", "248-248-2489", "friend");
+
+// print(addressBook);
+// console.log("********");
+// addressBook.deleteAt(1); //delete Tyler
+// print(addressBook);
+// // Collapse;
+
+function display() {
+  document.querySelector("#contact_list").innerHTML = "";
+  addressBook.contacts.forEach((contact, index) => {
+    const newEntry = document.createElement("div");
+    newEntry.classList.add("contact_box");
+    newEntry.innerHTML = `
+    <p>Name: ${contact.name}</p>
+    <p>Email: ${contact.email}</p>
+    <p>Phone: ${contact.phone}</p>
+    <p>Relation: ${contact.relation}</p>
+    <i class="fa fa-trash" data-index-numbers="${index}"
+    aria-hidden= "true"></li>
+    `;
+    document.querySelector("#contact_list").appendChild(newEntry);
+  });
+}
+display();
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", addContact);
+
+function addContact(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  addressBook.add(
+    formData.get("name"),
+    formData.get("email"),
+    formData.get("phone"),
+    formData.get("relation")
+  );
+  form.reset();
+  display();
+}
+
+document
+  .querySelector("#contact_list")
+  .addEventListener("click", deleteContact);
+
+function deleteContact(e) {
+  if (e.target.classList.contains("fa-trash")) {
+    const index = e.target.getAttribute("data-index-number");
+    console.log(index);
+    addressBook.deleteAt(index);
+    display();
+  }
+}
